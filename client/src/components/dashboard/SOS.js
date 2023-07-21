5e776fe35536b28a55b82558ab423c9de1d3f0fc
aarushi.abrol <aarushi.abrol@webners.com>
2023-07-21 12:27:11 +0530

    card css fixed

--- client/src/components/dashboard/SOS.js
+++ client/src/components/dashboard/SOS.js
@@ -2,22 +2,23 @@ import "../../assets/css/sos.css";
 import NavbarInside from './navbar-inside';
 import SOSCard from "./SOS-card";
 const SOS = () => {
-    return(
-        <div className='home-outer-sos' style={{display:'flex', flexDirection:'column'}}>
-        <div>
-          <NavbarInside />
-        </div>
-        <center><h1 style={{ marginTop: '200px', color:'white' }}>SOS Received</h1></center>
-        <div style={{margin:'3vh'}}>
+  return (
+    <div className='home-outer-sos'>
+      <div>
+        <NavbarInside />
+      </div>
+      <center><h1 className="sos-heading">SOS Received</h1></center>
+      <div className="cards">
         <SOSCard
-        policeName="Ram Mishra"
-        badgeId="12345XXX"
-        duration="3 hours"
-        location="Delhi"
-        description="A stranger with a black bagpack, is constantly travelling by the police station, overlooking to follow me"
-      />
-        </div>
-     </div>
-)}
+          policeName="Ram Mishra"
+          badgeId="12345XXX"
+          duration="3 hours"
+          location="Delhi"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        />
+      </div>
+    </div>
+  )
+}
 
 export default SOS
