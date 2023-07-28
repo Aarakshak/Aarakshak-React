0be90f6f2ca55342e2467a174423c13bf9944238
Prateek Rustagi <prateekrustagi2001@gmail.com>
2023-07-28 19:05:14 +0530

    corrected notifications page

--- client/src/components/dashboard/work-notif.js
+++ client/src/components/dashboard/work-notif.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import avatar from '../../assets/logos/shg.svg';
+import avatarid from '../../assets/logos/id.png';
+import timer from '../../assets/logos/time.png';
 import NavbarInside from './navbar-inside2';
 import '../../assets/css/home.css';
 
@@ -10,15 +12,9 @@ const WorkNotifs = () => {
   const [time, setTime] = useState('');
   const [description, setDescription] = useState('');
   const [choiceValue, setChoiceValue] = useState('');
-
-  const choice = [
-    {value: "0", text: "Select"}, 
-    {value: "1", text: "USA"}, 
-    {value: "2", text: "Canada"}, 
-    {value: "3", text: "Japan"}, 
-    {value: "4", text: "Mexico"}
-  ]
-
+  const [uniqueId, setUniqueId] = useState('');
+  const [choiceValue2, setChoiceValue2] = useState('');
+  
   const onSubmit = (e) => {
     e.preventDefault();
 
@@ -44,12 +40,12 @@ const WorkNotifs = () => {
       <div>
         <NavbarInside />
       </div>
-      <div className='wrapper wrapper22 wrapper76' style={{ marginTop: '200px' }}>
+      <div className='wrapper wrapper22 wrapper76 wrapper77' style={{ marginTop: '200px' }}>
         <h1 className='heading-settings'>Post Work Notifications</h1>
           <div className='settings-box'>
-            <form>
+            <form className='form-notifs'> 
               <div className='row'>
-              <div className='form-group col-sm-6'>
+              <div className='col-sm-6'>
                   <label htmlFor='title'>
                     Notification Title :{' '}
                   </label>
@@ -64,18 +60,45 @@ const WorkNotifs = () => {
                     onChange={(e) => setTitle(e.target.value)}
                   />
                 </div>                
-                <div className='form-group col-sm-6'>
+                <div className='col-sm-6'>
                   <label htmlFor='location'>Type : </label>
                     <select value={choiceValue} onChange={(e) => setChoiceValue(e.target.value)}>
                         <option onClick={() => setChoiceValue("1")}> Meeting </option>
                         <option onClick={() => setChoiceValue("2")}> Duty </option>
                         <option onClick={() => setChoiceValue("3")}> Local Events </option>
-                        <option onClick={() => setChoiceValue("4")}> Others </option>
+                        <option onClick={() => setChoiceValue("4")}> Emergency </option>
+                        <option onClick={() => setChoiceValue("5")}> Others </option>
                     </select>
                 </div>
               </div>
+
               <div className='row'>
-                <div className='form-group col-sm-6'>
+              <div className='col-sm-6'>
+                  <label htmlFor='location'>Post to : </label>
+                    <select value={choiceValue2} onChange={(e) => setChoiceValue2(e.target.value)}>
+                        <option onClick={() => setChoiceValue2("1")}> Police Officer </option>
+                        <option onClick={() => setChoiceValue2("2")}> Police Station </option>
+                    </select>
+                </div>
+                <div className='col-sm-6'>
+                    <label htmlFor='title'>
+                      Unique ID :{' '}
+                    </label>
+                    <span>
+                      <img alt='' className='updater updater-notif' src={avatarid}></img>
+                    </span>
+                    <input
+                      type='number'
+                      name='uniqueid'
+                      placeholder= 'Enter Unique ID'
+                      value={uniqueId}
+                      onChange={(e) => setUniqueId(e.target.value)}
+                    />
+                  </div>                
+              </div>
+
+              <div className='row'>
+                <div className='col-sm-6'>
                   <label htmlFor='date'> Date of Event : </label>
                   <input
                     type='date'
@@ -85,8 +108,11 @@ const WorkNotifs = () => {
                     onChange={(e) => setDate(e.target.value)}
                   />
                 </div>
-                <div className='form-group col-sm-6'>
+                <div className='col-sm-6'>
                   <label htmlFor='time'> Time of Event : </label>
+                  <span>
+                      <img alt='' className='updater updater-notif' src={timer}></img>
+                  </span>
                   <input
                     type='time'
                     name='time'
@@ -97,7 +123,7 @@ const WorkNotifs = () => {
                 </div>
               </div>    
               <div className='row'>
-                <div className='form-group col-sm-12'>
+                <div className='col-sm-12'>
                 <label htmlFor='time'> Description : </label>
                 <textarea value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
                 </div>
